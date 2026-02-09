2128a53afeda8573d68134f528f494dc39fa5583
jxjyu <jxjyu@student.ubc.ca>
2026-02-08 21:44:47 -0800

    0.1/feat: basic model functionality

---
+++ src/main/model/PrairieOprah.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.util.ArrayList;
+
+public class PrairieOprah {
+    private ArrayList<Entry> entries;
+    private ArrayList<Passcode> passcodes;
+    private ArrayList<Passcode> expired;
+
+    public PrairieOprah() {
+        entries = new ArrayList<>();
+        passcodes = new ArrayList<>();
+        expired = new ArrayList<>();
+    }
+
+    public boolean checkEntry(Entry e) {
+        for (Passcode p : passcodes) {
+            if (p.good(e.getPasscode())) {
+                entries.add(e);
+                return true;
+            }
+            if (p.isExpired()) {
+                expired.add(p);
+                passcodes.remove(p);
+            }
+        }
+        return false;
+    }
+
+    public void createPasscode(int minutes) {
+        passcodes.add(new Passcode(minutes));
+    }
+}
